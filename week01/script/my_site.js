44bd4941563d9a0c3a3cb0f3c7b40f5033477f5c
Francogallardo99 <gal24016@byui.edu>
2025-09-07 00:50:46 -0300

    almost ready

--- week01/script/my_site.js
+++ week01/script/my_site.js
@@ -8,3 +8,14 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('visible');
 });
+
+let date = new Date();
+let year = date.getFullYear();
+let span = document.getElementById("currentyear");
+if (span) {
+    span.textContent = year;
+    console.log("Year set:", year); 
+
+}
+let lastModifiedParagraph = document.getElementById('lastModified');
+lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
